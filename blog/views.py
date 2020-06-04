0133f9462e166631ec1199316942b8002f344db4
Rakesh Nalumachu <nalumachu.rakesh@gmail.com>
2020-06-04 08:56:04 -0400

    delete blog

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from .forms import BlogPostForm, BlogPostModelForm
@@ -100,5 +100,8 @@ def blog_post_update_view(request, slug):
 def blog_post_delete_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
     template_name = 'blog/delete.html'
+    if request.method == "POST":
+        obj.delete()
+        return redirect("/blog")
     context = {"object": obj}
     return render(request, template_name, context)
