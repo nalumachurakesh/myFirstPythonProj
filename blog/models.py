e196e70a9732e29d4082ada77b33f83bc0c9919e
Rakesh Nalumachu <nalumachu.rakesh@gmail.com>
2020-06-14 18:11:30 -0400

    Static files and uploading files

--- blog/models.py
+++ blog/models.py
@@ -36,6 +36,7 @@ class BlogPost(models.Model):
     # on_delete=models.CASCADE
     
     user = models.ForeignKey(User, default=1, null=True,  on_delete=models.SET_NULL)
+    image = models.FileField(upload_to='image/', blank=True, null=True)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True) # hello world -> hello-world
     content = models.TextField(null=True, blank=True)
