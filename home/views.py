a3dc67827c123b6bd700a173622e6b3dc3668814
Rakesh Nalumachu <nalumachu.rakesh@gmail.com>
2020-06-14 15:23:58 -0400

    beautifying blogs display

--- home/views.py
+++ home/views.py
@@ -3,13 +3,15 @@ from django.shortcuts import render
 from django.template.loader import get_template
 
 from .forms import ContactForm
+from blog.models import BlogPost
 
 
 def home_page(request):
     my_title = "Hello there...."
-    context = {"title": my_title}
-    if request.user.is_authenticated:
-        context = {"title": my_title, "my_list": [1, 2, 3, 4, 5]}
+    qs = BlogPost.objects.all()[:5] # [:5] - Will only give you 5 items from a list
+    context = {"title": my_title, 'blog_list': qs}
+    # if request.user.is_authenticated:
+    #     context = {"title": my_title, "my_list": [1, 2, 3, 4, 5]}
     # doc = "<h1>{title}</h1>".format(title=my_title)
     # django_rendered_doc = "<h1>{{title}}</h1>".format(title=my_title)
     return render(request, "home.html", context)
